ede842594101915b3e5348ea3b47c5d9a38fa164
conanskyforce <conanskyforce@163.com>
2019-04-23 14:11:01 +0800

    del extra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import {Switch, Route} from 'react-router-dom'
 import './App.css';
+import './styles/header.css';
+import './styles/normalize.css';
+import './styles/my-item.css';
+import './styles/footer.css';
+import './styles/loading.css';
+import './styles/project.css';
+import './styles/page-navagation.css';
+import MyHeader from './components/my-header'
+import MyContent from './components/my-content'
+import MyFooter from './components/my-footer'
+import MyProject from './components/my-project'
+
+import PropTypes from 'prop-types'
 
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			themeColor:'white'
+		}
+	}
+	static childContextTypes = {
+		themeColor:PropTypes.string
+	}
+	getChildContext(){
+		return {
+			themeColor:this.state.themeColor
+		}
+	}
+	componentDidMount(){
+	}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='all-wrapper'>
+      <MyHeader />
+      	<Switch>
+      		<Route exact path='/' component={MyContent} />
+      		<Route path='/:project' component={MyProject} />
+      	</Switch>
+      <MyFooter />
       </div>
     );
   }
